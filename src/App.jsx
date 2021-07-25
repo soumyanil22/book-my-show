0b4cc5b53f9f9b657982e5c8580d7e832ec5aab0
soumyanil22 <soumyanil22@gmail.com>
2021-07-25 09:57:47 +0530

    fixed plays page

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,7 @@ function App() {
     <>
       <DefaultHOC path="/" exact component={HomePage} />
       <MovietHOC path="/movie/:id" exact component={Movie} />
-      <DefaultHOC path="/plays/" exact component={Plays} />
+      <DefaultHOC path="/plays" exact component={Plays} />
     </>
   );
 }
